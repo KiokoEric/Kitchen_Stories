2bde97d9a72970710cbc2b32c65a1d9e9a7df17a
kiokoeric <kiokoerick040@gmail.com>
2024-06-14 15:17:32 +0300

    Introduction of user configuration to the Header Component in the Kitchen Stories Website

--- Client/Kitchen_Stories/src/Components/Header/Header.tsx
+++ Client/Kitchen_Stories/src/Components/Header/Header.tsx
@@ -102,7 +102,7 @@ return (
         }
             <Link to={`/Profile/${UserID}`} >
                 <FaUser size="2rem" className="bg-black text-white cursor-pointer px-1.5 py-1.5 rounded-full" />
-                </Link>
+            </Link>
                 {UserID ? <h4 className="font-bold"><span>Welcome</span>{Name}</h4> : null }
         </section>
     </div>
