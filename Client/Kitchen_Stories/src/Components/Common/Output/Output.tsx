3828f2c3590af8bcac1b12b428644d206fc83a69
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-19 23:01:50 +0300

    Introduction of additional props in the reusable Output component.

--- Client/Kitchen_Stories/src/Components/Common/Output/Output.tsx
+++ Client/Kitchen_Stories/src/Components/Common/Output/Output.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 interface ResultProps {
-    figureStyle: string;
     image: string;
+    Title: string;
     imageStyle: string;
     TitleStyle: string;
-    Title: string;
+    figureStyle: string;
 }
 
 const Output: React.FC<ResultProps> = ({ figureStyle, image, imageStyle, TitleStyle, Title }) => {
 return (
     <figure className={figureStyle} >
-        <img src={image} alt="" width="500px" className={imageStyle} /> 
+        <img src={image} alt="" className={imageStyle} /> 
         <figcaption>
             <h2 className={TitleStyle}>{Title}</h2>
         </figcaption>
