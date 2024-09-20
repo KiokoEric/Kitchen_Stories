7f084771d7c594d0733d64a8ed7e4f5df40730cc
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-20 22:25:58 +0300

    Creation of the Error Boundary Page in the Kitchen Stories Website.

--- Client/Kitchen_Stories/src/Pages/ErrorBoundary/ErrorBoundary.tsx
+++ Client/Kitchen_Stories/src/Pages/ErrorBoundary/ErrorBoundary.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ErrorBoundary: React.FC = () => {
 return (
-    <h1 className='font-bold mt-5 text-red-600 text-3xl text-center'>Kindly Login to access the Kitchen Stories Resources.</h1>
+    <h1 className='font-bold mx-auto mt-5 text-red-600 text-3xl text-center w-screen'>Kindly Login to access the Kitchen Stories Resources.</h1>
 )
 }
 
