b31e5c8d83fb95a842c276ffd336109fd69f658f
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-20 23:32:52 +0300

    Modification of the My Recipes Page to be responsive in both mobile view and tablet view.

--- Client/Kitchen_Stories/src/Pages/MyRecipes/MyRecipes.tsx
+++ Client/Kitchen_Stories/src/Pages/MyRecipes/MyRecipes.tsx
@@ -1,21 +1,32 @@
-import Axios from "axios";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import React, { useEffect, useState } from 'react';
-import { MdDelete } from "react-icons/md";
-import { MdEditSquare } from "react-icons/md";
+import { useCookies } from "react-cookie";
 import loadingGif from "../../assets/Cooking.gif";
-import Heading from "../../Components/Common/Heading/Heading";
+import React, { useEffect, useState } from 'react';
 import Output from "../../Components/Common/Output/Output";
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import Heading from "../../Components/Common/Heading/Heading";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { useGetUserID } from "../../Components/Hooks/useGetUserID";
 
 const MyRecipes: React.FC = () => {
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [Recipes, setRecipes] = useState([]) 
+    const userID = useGetUserID();
+    const [Cookie, _] = useCookies(["auth_token"]);
+
+    // USESTATE HOOK
+
+    const [Recipes, setRecipes] = useState<[]>([]) 
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    // CALLING ON THE USER'S CREATED RECIPES
 
     useEffect(() => {
 
         const fetchRecipes = async () => {
-            await Axios.get(``, {
+            await axios.get(`http://localhost:4000/Recipe/${userID}/Recipes`, {
+            headers: { authorization: Cookie.auth_token },
             }) 
             .then((Response) => {
                 setRecipes(Response.data)
@@ -25,45 +36,62 @@ const MyRecipes: React.FC = () => {
             }, 1500);
         }
 
-        fetchRecipes
-    })
+        fetchRecipes() 
+
+    }, [userID])
+
+    // DELETE RECIPE
+
+    const handleDelete= (_id: any) => {
+        axios.delete(`http://localhost:4000/Recipe/${_id}`, {
+            headers: { authorization: Cookie.auth_token }
+        }) 
+        .then(
+            window.location.reload()
+        )
+    }
 
 return (
-    <div>
+    <div className="mb-10" >
         <Heading
             idName='MyRecipes'
             ContainerStyle='flex flex-col items-center justify-center gap-5 mb-8 text-center text-white'
             Heading='My Recipes'
             HeadingStyle='font-bold text-5xl'
         />
-        <section className='flex flex-auto items-center justify-center gap-5 px-10'>
-        {isLoading ? (
-            <div className='flex flex-col items-center justify-center'>
-                <img src={loadingGif} alt="Loading Gif..." className="w-max" />
-            </div>
+        <section className='grid grid-cols-1 items-center justify-center gap-8 m-auto w-11/12 sm:grid-cols-3'>
+            {isLoading ? (
+                <div className="flex flex-col items-center justify-center m-auto w-custom">
+                    <img src={loadingGif} alt="Loading Gif..." id="Loading" />
+                </div>
             ) : (
             (Recipes.length > 0) ?  
             Recipes.map((Recipe : any) => { 
             return (
-            <div key={Recipe.index} >
-                <Link to={`/Information/${Recipe._id}`} className='flex flex-col gap-3 text-black no-underline'> 
-                <Output
-                    figureStyle='flex flex-col gap-5 mb-5'
-                    image={Recipe.Image}
-                    imageStyle='rounded w-11/12'
-                    TitleStyle='capitalize font-bold text-black text-center text-3xl'
-                    Title={Recipe.Name}
-                />
-                    <div className="flex gap-3 justify-end">
-                        <MdEditSquare size="1rem" className="bg-Orange" color="black" />
-                        <MdDelete size="1rem" className="bg-Orange" color="black" />
-                    </div>
+            <div className="flex flex-col items-center justify-center" >
+                <Link key={Recipe.index}  to={`/Information/${Recipe._id}`} className='flex flex-col gap-3 text-black no-underline'> 
+                    <Output
+                        figureStyle='flex flex-col gap-5 mb-5'
+                        image={Recipe.Image}
+                        imageStyle='rounded'
+                        TitleStyle='capitalize font-bold text-black text-center text-3xl'
+                        Title={Recipe.Name}
+                    />
                 </Link>
+                <div className="flex gap-3 items-center justify-center">
+                    <Link id="Edit" to={`/Edit/${Recipe._id}`} key={Recipe._id}>
+                        <FontAwesomeIcon icon={faPenToSquare} className="bg-orange-600 cursor-pointer font-bold p-3 rounded-full text-xl" />
+                    </Link>
+                    <div id="Delete">
+                        <FontAwesomeIcon icon={faTrash} className="bg-orange-600 cursor-pointer font-bold p-3 rounded-full text-xl" onClick={() => handleDelete(Recipe._id)} /> 
+                    </div>
+                    
+                </div>
             </div>
             )
-            }) : <h2 className='font-bold text-red-700 text-center text-3xl'>No Results Found</h2>
-            )
-            }
+            })
+            : <h2 className='font-bold m-auto text-red-700 text-center text-5xl w-custom'>No Results Found</h2>
+            )}
         </section>
     </div> 
 )
