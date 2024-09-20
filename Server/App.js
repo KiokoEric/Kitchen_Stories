64d1a8e5b1e242b1e15e3fb9a5f8328f56d65568
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-21 00:47:04 +0300

    Modification of User Route in the Kitchen Stories Server Side.

--- Server/App.js
+++ Server/App.js
@@ -1,38 +1,37 @@
+const cors = require('cors');
+const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const dotenv = require('dotenv');
 const app = express();
 
 dotenv.config();
 
-// Middleware
+// MIDDLEWARE
 
 app.use(cors({
     origin: ["http://localhost:5173"],
-    methods: ["POST", "GET", "PUT" ,"DELETE"],  
+    methods: ["POST", "GET", "PUT" ,"DELETE"],    
     credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
 
-// MongoDB Connection    
+// MONGODB CONNECTION     
 
-const dbUrl = process.env.MONGODB_URL
+const dbUrl = process.env.MONGODB_URL 
 
 mongoose.connect(dbUrl)
 .then(() => console.log("Connected to the database!"))
 
-// Import Routes
+// IMPORT ROUTES
 
 const UserRoute = require("./Routes/UserRoute");
 const RecipeRoute = require("./Routes/RecipeRoute"); 
 const FavouriteRoute = require("./Routes/FavouritesRoute"); 
 
-
 app.use("/Users", UserRoute);
 app.use("/Recipe", RecipeRoute);
-app.use("/Favourites", FavouriteRoute);  
+app.use("/Favourites", FavouriteRoute);   
 
 app.listen(4000)
