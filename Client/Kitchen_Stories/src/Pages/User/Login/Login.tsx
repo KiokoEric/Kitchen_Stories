5d5d3a78b6bc7060db56fd7e7f9dbc00b3091606
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-24 19:10:29 +0300

    Update of the Login Page

--- Client/Kitchen_Stories/src/Pages/User/Login/Login.tsx
+++ Client/Kitchen_Stories/src/Pages/User/Login/Login.tsx
@@ -87,12 +87,12 @@ const Login: React.FC  = () => {
                 <div className='flex gap-5 mt-5'>
                     <Button
                         ButtonText='Login'
-                        ButtonStyle='bg-black cursor-pointer text-center text-white px-3 py-1 rounded w-40'
+                        ButtonStyle='bg-buttonOrange cursor-pointer text-center text-white px-3 py-1 rounded w-40 hover:bg-black'
                         onClick={handleSubmit(onLogin)}
                     />
                     <Button
                         ButtonText='Demo Login'
-                        ButtonStyle='bg-black cursor-pointer text-center text-white px-3 py-1 rounded w-40'
+                        ButtonStyle='bg-buttonOrange cursor-pointer text-center text-white px-3 py-1 rounded w-40 hover:bg-black'
                         onClick={DemoLogin}
                     />
                 </div>
